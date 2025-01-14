7ee9c5a19c3a192f603430fae60a7c920effdd91
rajveer-web <85809970+rajveer-web@users.noreply.github.com>
2025-01-15 11:29:50 +1300

    Update script.js

--- sound board/script.js
+++ sound board/script.js
@@ -1,11 +1,21 @@
-const labels = document.querySelectorAll(".form-control label");
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+const buttons = document.getElementById("buttons");
 
-labels.forEach((label) => {
-  label.innerHTML = label.innerText
-    .split("")
-    .map(
-      (letter, idx) =>
-        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
-    )
-    .join("");
+const stopSounds = () => {
+  sounds.forEach((sound) => {
+    const currentSound = document.getElementById(sound);
+    currentSound.pause();
+    currentSound.currentTime = 0;
+  });
+};
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.innerText = sound;
+  btn.addEventListener("click", () => {
+    stopSounds();
+    document.getElementById(sound).play();
+  });
+  buttons.appendChild(btn);
 });
